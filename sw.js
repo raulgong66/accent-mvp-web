8d7bc3080c3c4b6a14fe9b5668cdf956656f3db8
raulgong66 <raul.gongora66@gmail.com>
2026-04-12 13:21:47 +0200

    Convert to PWA: add manifest, service worker and brand-aligned icons

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'accent-detector-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './es.html',
+    './css/style.css',
+    './js/globe.js',
+    './js/recorder.js',
+    './manifest.json',
+    './images/icon-192.png',
+    './images/icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching static assets');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Event
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys
+                .filter(key => key !== CACHE_NAME)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
+});
